d20bc85d0d27febf5acd138d075ce5b8123dc782
Taby <tabysother@gmail.com>
2024-07-11 10:26:09 -0500

    getting the url I'm gonna probably use

---
+++ app.py
@@ -0,0 +1,14 @@
+import requests
+
+pokeapi = "https://pokeapi.co/api/v2/"
+
+pokemon = input("What pokemon do you wanna look up?")
+pokemonUrl = f"pokemon/{pokemon}"
+pokedexUrl = f"pokemon-species/{pokemon}"
+
+res = requests.get(f"{pokeapi}{pokemonUrl}")
+resPokedex = requests.get(f"{pokeapi}{pokedexUrl}")
+
+
+print(res.url)
+print(resPokedex.url)
