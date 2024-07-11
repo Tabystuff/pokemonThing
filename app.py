840448770df476f9e76027d4e47917308030ee9d
Taby <tabysother@gmail.com>
2024-07-11 10:37:48 -0500

    got the pokedex entries, but it has new lines inserted so gonna have to try and fix that

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import requests
 
 pokeapi = "https://pokeapi.co/api/v2/"
 
-pokemon = input("What pokemon do you wanna look up?")
+pokemon = input("What pokemon do you wanna look up? ")
 pokemonUrl = f"pokemon/{pokemon}"
 pokedexUrl = f"pokemon-species/{pokemon}"
 
@@ -12,3 +12,4 @@ resPokedex = requests.get(f"{pokeapi}{pokedexUrl}")
 
 print(res.url)
 print(resPokedex.url)
+print(resPokedex.json()["flavor_text_entries"][3]["flavor_text"]) 
